e7148439e98965267b58b4783e22a5ef0b99fdd6
Luke <Luke@lukes-mbp.home>
2020-12-04 17:35:25 +0000

    problem 1
    have begun working on the solution to problem 1 on Project Euler

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
+# If we list all the natural numbers below 10 that are
+# multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def muiltipleThree():
+
+    three_total = 0
+    n = 0
+
+    while 3 * n < 1000:
+        n += 1
+        num = 3 * n
+        three_total += num
+    return three_total
+
+    print(three_total)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def muiltipleFive():
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    five_total = 0
+    x = 0
+
+    while 5 * x < 1000:
+        x += 1
+        num = 5 * x
+        five_total += num
+    return five_total
+
+    print(five_total)
+
+
+print(muiltipleFive())
+print(muiltipleThree())
+
+print(muiltipleFive() + muiltipleThree())
+
+
+def compute():
+	ans = sum(x for x in range(1000) if (x % 3 == 0 or x % 5 == 0))
+	return str(ans)
+
+
+if __name__ == "__main__":
+	print(compute())
