febb1f18c71e6003caaebc8eb9d6195f7f8a1547
Luke <Luke@Lukes-MacBook-Pro.local>
2020-12-16 18:02:50 +0000

    files added
    added some files for my solutions and completed the first MultipleSum challenge

---
+++ MultipleSum.py
@@ -0,0 +1,36 @@
+# 1. Multiples of 3 and 5
+
+# If we list all the natural numbers below 10 that are
+# multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+
+
+# OFFICIAL SOLUTION
+
+# def compute():
+# 	ans = sum(x for x in range(1000) if (x % 3 == 0 or x % 5 == 0))
+# 	return str(ans)
+#
+#
+# if __name__ == "__main__":
+# 	print(compute())
+
+
+
+
+
+def muiltipleSum():
+
+    total = 0
+    x = 0
+
+    while x < 1000:
+        if x % 5 == 0 or x % 3 == 0:
+            total += x
+        x += 1
+    return total
+
+    print(total)
+
+print(muiltipleSum())
