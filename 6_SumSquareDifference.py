9b810f3f4c36c24f263289647460aeca3a5a5a78
Luke <Luke@lukes-mbp.home>
2020-12-30 17:14:25 +0000

    completed solution to Q6

--- 6_SumSquareDifference.py
+++ 6_SumSquareDifference.py
@@ -7,3 +7,18 @@
 #
 # Find the difference between the sum of the squares of the
 # first one hundred natural numbers and the square of the sum.
+
+def calculator():
+
+    total = 0
+    sq = 0
+
+    for x in range(1, 101):
+        num = x * x
+        total += num
+        sq += x
+        print(sq)
+    return ((sq * sq) - total)
+
+
+print(calculator())
